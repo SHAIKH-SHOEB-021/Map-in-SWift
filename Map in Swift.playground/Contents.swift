849e9c6fb672df7601d6b5a79cafe0cbc82821d1
vinaygupta.cont <vinaygupta.cont@niveussolutions.com>
2022-12-31 10:58:03 +0530

    Map Concept Done

--- Map in Swift.playground/Contents.swift
+++ Map in Swift.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let withOptional = [1,2,3,4,5,6,nil,8,9,10]
+//This is multi Dimensional Array
+let withOutOptional: [[Int]] = [[1,2,3,4,5],[6,7,8,9,10]]
+
+//Map if  without optional print array so used to ?? like $0 ?? 0
+var mapS = withOptional.map({
+    $0
+})
+print(mapS)
+
+//CompactMap
+var compectMAP = withOptional.compactMap({vale in
+    return vale
+})
+print(compectMAP)
+
+//FlatMap
+func FlatMapPrint() -> [Int] {
+    return withOutOptional.flatMap({$0})
+}
+print(FlatMapPrint())
